9d644bb1819dbdea8ae6adb0b86ec410af2cab86
Radhe0777 <princek8356@gmail.com>
2024-10-12 11:17:20 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -117,7 +117,7 @@ SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/ur_rishu_143"
 )  # Example:- https://t.me/RishuTeam
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/ur_support07"
+    "SUPPORT_GROUP", "https://t.me/FRIENDZONEOFFICIALL"
 )  # Example:- https://t.me/RishuNetwork 
 
 SUPPORT_CHAT = getenv(
