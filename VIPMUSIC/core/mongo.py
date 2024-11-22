d90a02d974d53ad6b553186738cf2621263721a5
aradhyamusic <aradhyamusicbot@gmail.com>
2024-11-22 11:00:01 +0530

    Update mongo.py

--- VIPMUSIC/core/mongo.py
+++ VIPMUSIC/core/mongo.py
@@ -16,7 +16,7 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://kuldiprathod2003:kuldiprathod2003@cluster0.wxqpikp.mongodb.net/?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://sevexedits:5JyQbk44RuB2jQj6@cluster0.lezcv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 if config.MONGO_DB_URI is None:
