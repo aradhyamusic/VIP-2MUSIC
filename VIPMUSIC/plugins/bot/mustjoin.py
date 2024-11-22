138108860dd6d7f8edb7350a200174d7c290ad62
aradhyamusic <aradhyamusicbot@gmail.com>
2024-11-22 11:10:11 +0530

    Update mustjoin.py

--- VIPMUSIC/plugins/bot/mustjoin.py
+++ VIPMUSIC/plugins/bot/mustjoin.py
@@ -27,7 +27,7 @@ MISHI = [
 
 #--------------------------
 
-MUST_JOIN = "NenoBots"
+MUST_JOIN = "ll_BOTCHAMBER_ll"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -47,8 +47,8 @@ async def must_join_channel(app: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/NenoBots"), 
-                                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/DP_WORLD7"), 
+                                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/ll_BOTCHAMBER_ll"), 
+                                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/ll_BOTCHAMBER_ll"), 
                             ]
                         ]
                     )
